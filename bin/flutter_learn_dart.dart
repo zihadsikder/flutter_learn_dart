a90779b37f2c091e8fa274aba5f34676e01cecb7
zihadsikder <zihadsikder.cse@gmail.com>
2023-09-16 00:19:40 +0600

    learn dart

---
+++ bin/flutter_learn_dart.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_learn_dart/flutter_learn_dart.dart' as flutter_learn_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_learn_dart.calculate()}!');
+}
---
+++ test/flutter_learn_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_learn_dart/flutter_learn_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
